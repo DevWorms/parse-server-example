d775e317737d6156f4c0c299618f38ae510d7ae1
Richard VelRo <rk_rivero@outlook.com>
2017-11-10 23:31:54 -0600

    Cambio pendejo

--- index.js
+++ index.js
@@ -38,23 +38,23 @@ var api = ParseServer({
   },
  
  emailAdapter: {
- module: 'parse-server-simple-mailgun-adapter',
- options: {
- fromAddress: process.env.EMAIL_FROM || "jaime@devolada.com",
- domain: process.env.MAILGUN_DOMAIN || "devolada.com",
- apiKey: process.env.MAILGUN_API_KEY || "key-938599f888f6225510f33735fb41557c",
- // Verification email subject
- verificationSubject: 'Please verify your e-mail for %appname%',
- // Verification email body
- verificationBody: 'Hi,\n\nYou are being asked to confirm the e-mail address %email% with %appname%\n\nClick here to confirm it:\n%link%',
+   module: 'parse-server-simple-mailgun-adapter',
+   options: {
+     fromAddress: process.env.EMAIL_FROM || "jaime@devolada.com",
+     domain: process.env.MAILGUN_DOMAIN || "devolada.com",
+     apiKey: process.env.MAILGUN_API_KEY || "key-938599f888f6225510f33735fb41557c",
+     // Verification email subject
+     verificationSubject: 'Please verify your e-mail for %appname%',
+     // Verification email body
+     verificationBody: 'Hi,\n\nYou are being asked to confirm the e-mail address %email% with %appname%\n\nClick here to confirm it:\n%link%',
 
-// Password reset email subject
- passwordResetSubject: 'Password Reset Request for %appname%',
- // Password reset email body
- passwordResetBody: 'Hi,\n\nYou requested a password reset for %appname%.\n\nClick here to reset it:\n%link%',
- //OPTIONAL (will send HTML version of email):
- passwordResetBodyHTML: "<!--DOCTYPE html>........"
- }
+    // Password reset email subject
+     passwordResetSubject: 'Password Reset Request for %appname%',
+     // Password reset email body
+     passwordResetBody: 'Hi,\n\nYou requested a password reset for %appname%.\n\nClick here to reset it:\n%link%',
+     //OPTIONAL (will send HTML version of email):
+     passwordResetBodyHTML: "<!--DOCTYPE html>........"
+   }
  }
 
 
@@ -78,11 +78,6 @@ app.get('/', function(req, res) {
   res.status(200).send('I dream of being a website.  Please star the parse-server repo on GitHub!');
 });
 
-// There will be a test page available on the /test path of your server url
-// Remove this before launching your app
-app.get('/test', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
-});
 
 var port = process.env.PORT || 1337;
 var httpServer = require('http').createServer(app);
